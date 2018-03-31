27ae491a047c36a14985dc380803516177fe8320
RASPWeather <rasp.github@hatcher.org.uk>
2018-03-31 13:59:55 +0100

    Adjuste permissions

--- morning_run.sh
+++ morning_run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # set this to the directory it runs in
-HOME_DIR=/home/rasp3/build/maxdist.0.4
+HOME_DIR=/home/rasp3/build/rpfd
 
 # now set them all off in parallel
 for MODEL in uk12 uk12+1 uk12+2 uk12+3 uk12+4 uk12+5 uk12+6
